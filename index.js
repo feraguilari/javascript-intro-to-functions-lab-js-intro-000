5f6689b92b4075e0d7cea82422499e6a26afa2fe
Fernando Aguilar Islas <f.aguislas@gmail.com>
2018-10-03 05:26:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,9 @@ function sayHiToGrandma(string){
     return "I can't hear you!"
   }
 }
+function sayHiToGrandma(string){
+  var uppercase = string.toUpperCase()
+  if (string === uppercase){
+    return "YES INDEED!"
+  }
+}
